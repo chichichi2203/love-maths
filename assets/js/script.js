85326d49e2689638db82763365889bbe6d19b10c
Ryan Blyth <ryan.blyth@hotmail.com>
2021-08-18 16:45:15 +0000

    Tidy up page for UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+    document.getElementById('answer-box').addEventListener('keydown', function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
 
     runGame("addition")
 });
@@ -23,6 +28,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the users' answer has been processed.
  */
 function runGame(gameType) {
+
+    document.getElementById('answer-box').value = "";
+    document.getElementById('answer-box').focus();
+
     // Creates two random numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
